eaa234ff3c4648cda5c5c3fca09da7a5381f87ce
Hamami <Hamami@DESKTOP-10O9TC3>
2020-03-31 15:42:11 +0300

    Added Admin Controller and remove unused import on Action Controller

--- src/main/java/demo/ActionController.java
+++ src/main/java/demo/ActionController.java
@@ -1,9 +1,7 @@
 package demo;
 
-import java.util.stream.*;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
--- src/main/java/demo/AdminController.java
+++ src/main/java/demo/AdminController.java
@@ -1,10 +1,11 @@
 package demo;
 
+import java.util.Collections;
+import java.util.Date;
 import java.util.stream.*;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,57 +15,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 	// Delete all users in the system
-	
-	@RequestMapping(path = "/acs/admin/users/{adminDomain}/{adminEmail}", 
-					method = RequestMethod.DELETE)
+
+	@RequestMapping(path = "/acs/admin/users/{adminDomain}/{adminEmail}", method = RequestMethod.DELETE)
 	public void delete_AllUsers(@PathVariable("adminDomain") String adminDomain,
-							 @PathVariable("adminEmail") String adminEmail
-			
-			) {} 
-	
-	
+			@PathVariable("adminEmail") String adminEmail
+
+	) {}
+
 	// Delete all elements in the system
 
-	@RequestMapping (path = "/acs/admin/elements/{adminDomain}/{adminEmail}", 
-			method = RequestMethod.DELETE)
-			public void delete_AllElements(
-					@PathVariable("adminDomain") String adminDomain,
-					 @PathVariable("adminEmail") String adminEmail){}
+	@RequestMapping(path = "/acs/admin/elements/{adminDomain}/{adminEmail}", method = RequestMethod.DELETE)
+	public void delete_AllElements(@PathVariable("adminDomain") String adminDomain,
+			@PathVariable("adminEmail") String adminEmail) {}
 	// Delete all actions in the system
 
-	@RequestMapping (path = "/acs/admin/actions/{adminDomain}/{adminEmail}", 
-			method = RequestMethod.DELETE)
-	public void delete_AllActions(
-			@PathVariable("adminDomain") String adminDomain,
-			 @PathVariable("adminEmail") String adminEmail)
-	{}
-	
-//	// Export all users
-//
-//	@RequestMapping (path = "/acs/admin/users/{adminDomain}/{adminEmail}",
-//					 method = RequestMethod.GET,
-//					 produces = MediaType.APPLICATION_JSON_VALUE)
-//	public UserBoundary[] exports_AllUsers (
-//	@PathVariable("adminDomain") String adminDomain,
-//	 @PathVariable("adminEmail") String adminEmail){
-//		return IntStream.range(0, 5) //stream of integers  // we using stream but we can use for as well
-//				.mapToObj(i -> "Neighborhood #" + (i+1))  //stream of strings
-//				.map(msg -> new NeighborhoodBoundary(msg, new String[]{"hadar","limon"},9.99)) // stream of UserBoundary
-//				.collect(Collectors.toList()) // list of UserBoundary
-//				.toArray(new UserBoundary[0]); //UserBoundary[]
-//	}
-//	// Export all actions
-//	@RequestMapping (path = "/acs/admin/actions/{adminDomain}/{adminEmail}",
-//			 method = RequestMethod.GET,
-//			 produces = MediaType.APPLICATION_JSON_VALUE)
-//public ActionBoundary[] exports_AllActions (
-//	@PathVariable("adminDomain") String adminDomain,
-//	 @PathVariable("adminEmail") String adminEmail)	) {
-//return IntStream.range(0, 5) //stream of integers  // we using stream but we can use for as well
-//		.mapToObj(i -> "User #" + (i+1))  //stream of strings
-//		.map(msg -> new ActionBoundary(msg, new String[]{"hadar","limon"},9.99)) // stream of ActionBoundary
-//		.collect(Collectors.toList()) // list of ActionBoundary
-//		.toArray(new ActionBoundary[0]); //ActionBoundary[]
-//}
-}
+	@RequestMapping(path = "/acs/admin/actions/{adminDomain}/{adminEmail}", method = RequestMethod.DELETE)
+	public void delete_AllActions(@PathVariable("adminDomain") String adminDomain,
+			@PathVariable("adminEmail") String adminEmail) {}
 
+	// Export all users
+
+	@RequestMapping (path = "/acs/admin/users/{adminDomain}/{adminEmail}",
+					 method = RequestMethod.GET,
+					 produces = MediaType.APPLICATION_JSON_VALUE)
+	public UserBoundry[] exports_AllUsers (
+	@PathVariable("adminDomain") String adminDomain,
+	 @PathVariable("adminEmail") String adminEmail){
+		return IntStream.range(0, 5) //stream of integers  // we using stream but we can use for as well
+				.mapToObj(i -> "User #" + (i+1))  //stream of strings
+				.map(msg -> new UserBoundry(Collections.singletonMap("2020b.ofir.cohen", "hamami2010@gmail.com"),msg,"user role",":)")) // stream of UserBoundary
+				.collect(Collectors.toList()) // list of UserBoundary
+				.toArray(new UserBoundry[0]); //UserBoundary[]
+	}
+
+	// Export all actions
+	@RequestMapping (path = "/acs/admin/actions/{adminDomain}/{adminEmail}",
+			 method = RequestMethod.GET,
+			 produces = MediaType.APPLICATION_JSON_VALUE)
+public ActionBoundary[] exports_AllActions (
+	@PathVariable("adminDomain") String adminDomain,
+	 @PathVariable("adminEmail") String adminEmail) {
+return IntStream.range(0, 5) //stream of integers  // we using stream but we can use for as well
+		.mapToObj(i -> "Action #" + (i+1))  //stream of strings
+		.map(msg -> new ActionBoundary(Collections.singletonMap("2020b.ofir.cohen", "971"),"actionType",Collections.singletonMap("domain", "54"),new Date(),Collections.singletonMap("invokedby:"+msg, "player@demo"),Collections.singletonMap("action", "information"))) // stream of ActionBoundary
+		.collect(Collectors.toList()) // list of ActionBoundary
+		.toArray(new ActionBoundary[0]); //ActionBoundary[]
+}
+}
