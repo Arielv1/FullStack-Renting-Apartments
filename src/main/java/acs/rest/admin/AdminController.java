d5c6f41dc2c8165a2a8678a288f11d7dac84bf05
Hamami <Hamami@DESKTOP-10O9TC3>
2020-05-01 15:13:30 +0300

    Fix Admin Controller , Working with Elements Database

--- src/main/java/acs/rest/admin/AdminController.java
+++ src/main/java/acs/rest/admin/AdminController.java
@@ -1,102 +1,98 @@
 package acs.rest.admin;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.*;
-
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import acs.data.UserRole;
+
+import acs.logic.action.ActionService;
+import acs.logic.db.DbElements;
 import acs.logic.element.ElementService;
+import acs.logic.user.UserService;
+import acs.logic.user.UserConvertor;
 import acs.rest.action.ActionBoundary;
-import acs.rest.action.boundaries.ActionElementBoundary;
-import acs.rest.action.boundaries.InvokedByBoundary;
 import acs.rest.users.UserBoundary;
-import acs.rest.utils.IdBoundary;
 
 //consumes =  type of input
 //produces =  type of output
 @RestController
 public class AdminController {
-	// Delete all users in the system
 	
+	private UserService userService;
+	private DbElements dbElementeService;
+	private ActionService actionService;
+
+	
+	// Delete all users in the system
+
 	@RequestMapping(path = "/acs/admin/users/{adminDomain}/{adminEmail}", method = RequestMethod.DELETE)
 	public void delete_AllUsers(@PathVariable("adminDomain") String adminDomain,
 			@PathVariable("adminEmail") String adminEmail
 
-	) {}
+	) {
+//		 this.userDao.deleteAll();
+		this.userService.deleteAllUsers(adminDomain, adminEmail);
+
+	}
 
 	// Delete all elements in the system
 	@RequestMapping(path = "/acs/admin/elements/{adminDomain}/{adminEmail}", method = RequestMethod.DELETE)
 	public void delete_AllElements(@PathVariable("adminDomain") String adminDomain,
-			@PathVariable("adminEmail") String adminEmail) {}
+			@PathVariable("adminEmail") String adminEmail) 
+	{
+//		 this.elementDao.deleteAll();
+		this.dbElementeService.deleteAllElements(adminDomain, adminEmail);
+	
+	}
 	
 	// Delete all actions in the system
 	@RequestMapping(path = "/acs/admin/actions/{adminDomain}/{adminEmail}", method = RequestMethod.DELETE)
+	
 	public void delete_AllActions(@PathVariable("adminDomain") String adminDomain,
-			@PathVariable("adminEmail") String adminEmail) {}
+			@PathVariable("adminEmail") String adminEmail) 
+	{
+//		this.actionDao.deleteAll();
+		this.actionService.deleteAllActions(adminDomain, adminEmail);
 
-	// Export all users
+	}
+
+	
+//	 Export all users
+	
 	@RequestMapping (path = "/acs/admin/users/{adminDomain}/{adminEmail}",
 					 method = RequestMethod.GET,
 					 produces = MediaType.APPLICATION_JSON_VALUE)
+	
 	public UserBoundary[] exports_AllUsers (
 	@PathVariable("adminDomain") String adminDomain,
 	 @PathVariable("adminEmail") String adminEmail){
-		Map <String, Object> userId = new HashMap <String, Object>();
-		userId.put("domain", "2020b.ofir.cohen");
-		userId.put("email", "MichaelHamami@gmail.com");
-		return IntStream.range(0, 5) //stream of integers  // we using stream but we can use for as well
-				.mapToObj(i -> "User #" + (i+1))  //stream of strings
-				.map(msg -> new UserBoundary(userId,"Demo User",UserRole.PLAYER,";-)")
-						) // stream of UserBoundary
-				.collect(Collectors.toList()) // list of UserBoundary
-				.toArray(new UserBoundary[0]); //UserBoundary[]
+
+	return this.userService.getAllUsers(adminDomain,adminEmail).toArray(new UserBoundary[0]);
 	}
 
+		
 	// Export all actions
 	@RequestMapping (path = "/acs/admin/actions/{adminDomain}/{adminEmail}",
 			 method = RequestMethod.GET,
 			 produces = MediaType.APPLICATION_JSON_VALUE)
 public ActionBoundary[] exports_AllActions (
 	@PathVariable("adminDomain") String adminDomain,
-	 @PathVariable("adminEmail") String adminEmail) {
-		Map <String, Object> actionId = new HashMap <String, Object>();
-		actionId.put("domain", "2020b.ofir.cohen");
-		actionId.put("id", "971");
-		
-		Map <String, Object> elementID = new HashMap <String, Object>();
-		elementID.put("domain", "2020b.ofir.cohen");
-		elementID.put("id", "54");
-		
-		Map <String, Object> element = new HashMap <String, Object>();
-		element.put("elementId", elementID);
-		
-		Map <String, Object> userID = new HashMap <String, Object>();
-		userID.put("domain", "2020b.ofir.cohen");
-		userID.put("email", "MichaelHamami@gmail.com");
-		
-		Map <String, Object> invokedBy = new HashMap <String, Object>();
-		invokedBy.put("userId", userID);
-		
-		Map <String, Object> actionAttributes = new HashMap <String, Object>();
-		actionAttributes.put("key1", "can be set to any value you wish");
-		actionAttributes.put("key2", 44.5);
-		actionAttributes.put("booleanValue", false);
-		actionAttributes.put("lastKey", "it can contain anything you wish");
-
-		
-return IntStream.range(0, 5) //stream of integers  // we using stream but we can use for as well
-		.mapToObj(i -> "Action #" + (i+1))  //stream of strings
-		.map(msg -> new ActionBoundary(
-				new IdBoundary(),"actionType",new ActionElementBoundary (),new Date(), new InvokedByBoundary(),actionAttributes)
-				) // stream of ActionBoundary
-		.collect(Collectors.toList()) // list of ActionBoundary
-		.toArray(new ActionBoundary[0]); //ActionBoundary[]
+	 @PathVariable("adminEmail") String adminEmail) 
+	{
+	
+	return this.actionService.getAllActions(adminDomain,adminEmail).toArray(new ActionBoundary[0]);
+	
+//		return StreamSupport
+//		.stream(
+//				// INVOKE SELECT DATABASE 
+//				this.actionDao
+//					.findAll()
+//					.spliterator(),
+//				false) //Stream<ActionEntity>
+//		.map(this.actionConverter::fromEntity) // Stream<ActionBoundary>
+//		.collect(Collectors.toList()) // List<ActionBoundary>
+//		.toArray(new UserBoundary [0]); //ActionBoundary[]
 }
 }
