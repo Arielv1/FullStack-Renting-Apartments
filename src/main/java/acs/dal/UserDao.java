7b990064394febf01f0e1201b85fc0c9d31a39aa
tomer2468 <tomer2468@DESKTOP-CHVR7C0>
2020-05-15 11:52:35 +0300

    user dao change data base

--- src/main/java/acs/dal/UserDao.java
+++ src/main/java/acs/dal/UserDao.java
@@ -1,22 +1,13 @@
 package acs.dal;
 
 
-import java.awt.print.Pageable;
-import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 import acs.data.users.UserEntity;
 import acs.data.utils.UserIdEntity;
 
 public interface UserDao extends PagingAndSortingRepository<UserEntity, UserIdEntity>{
-	/*
-	public List<UserEntity> findByUserId(
-			@Param ("userId") UserIdEntity userId, 
-			Pageable pageable);
-		*/
-	
 	
 
 }
